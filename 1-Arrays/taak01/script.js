be2eef935f487976a10d1b6ade32b60524e9a8b5
JuliandeVries <Julian.devries@student.rocva.nl>
2021-05-20 12:58:49 +0200

    f

---
+++ 1-Arrays/taak01/script.js
@@ -0,0 +1,9 @@
+$Voornaam['Piet'] = Piet;
+$Achternaam['Piet'] = Janssen;
+$Leeftijd['Piet'] = 18;
+$Klas['Piet'] = 8;
+
+$studenten = array("Piet"); 
+
+echo ; 
+
---
+++ 2-Loops/taak01/loops-while.php
@@ -0,0 +1,8 @@
+<?php
+$leeftijd = 0;
+
+while($leeftijd <18){
+    echo "Ik ben $leeftijd <br /> jaar, dus ik mag nog niet stemmen";
+    $leeftijd++;
+}
+?>
